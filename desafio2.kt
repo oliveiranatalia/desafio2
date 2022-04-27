26e0a6a6ee3c6b5b8e8aa480a6328146d005e7fd
oliveiranatalia <102336871+oliveiranatalia@users.noreply.github.com>
2022-04-26 21:36:48 -0300

    corrigir codigo

--- desafio2.kt
+++ desafio2.kt
@@ -1,58 +1,58 @@
-import java.lang.NumberFormatException
 import kotlin.system.exitProcess
+import java.lang.NumberFormatException
 
-//Programa que leia 4 notas e retorne a média de cada aluno usando métodos.
+//Programa que leia 4 notas e retorne a média de aluno usando métodos.
 
 fun main () {
     println("\n---------------------------------\n         média global\n---------------------------------")
-    nome()
+    resultado()
 }
 fun retorno(){
     try {
         print("\n-- 1 Começar novamente   2 Sair: ")
         val escolha = readln().toInt()
-        if (escolha == 1) {
-            nome()
-        } else if (escolha == 2){
-            exitProcess(0)
-        } else {
+        when(escolha){
+            1 -> resultado()
+            2 -> exitProcess(0)
+            else -> {
             print("-- valor inválido\n")
             retorno()
-        }
-    } catch(ex: NumberFormatException){
-        print("-- Insira apenas números\n")
-        retorno()
-    }
-}
-fun nome (){
-    print("\nInsira o nome do(a) aluno(a): ")
-    val aluno = readln()
-    notas(aluno)
-}
-fun notas(aluno: String) {
-    try {
-        var listaNotas = DoubleArray(4)
-        var ind = 1
-        var soma: Double = 0.0
-        for (nota: Double in listaNotas) {
-            print("Insira a $ind ª nota: ")
-            var nota = readln().toDouble()
-            ind += 1
-            if (nota < 0){
-                println("Valor inválido")
-                retorno()
-            } else {
-                soma += nota
             }
         }
-        resultado(aluno, soma)
-    } catch(ex: NumberFormatException){
+    }catch(ex: NumberFormatException){
         print("-- Insira apenas números\n")
         retorno()
     }
 }
-fun resultado(aluno: String, soma: Double) {
-    var media = soma / 4
-    print("A média final de $aluno é $media\n")
+fun nome (): String{
+    print("\nInsira o nome do(a) aluno(a): ")
+    val aluno = readln()
+    return aluno
+}
+fun notas(): Double {
+    //try {
+        val listaNotas = DoubleArray(4)
+        var ind = 1
+        var soma: Double = 0.0
+        for (i in listaNotas.indices) {
+            print("Insira a $ind ª nota: ")
+            val nota = readln().toDouble()
+            ind += 1
+            if (nota in 0.0 .. 10.0){
+                listaNotas[i]
+                soma += nota
+            } else {
+                println("Valor inválido")
+                retorno()
+            }
+        }
+        return soma
+    //} catch(ex: NumberFormatException){
+    //    print("-- Insira apenas números\n")
+    //    retorno()}
+}
+fun resultado() {
+    val media = notas() / 4
+    print("A média final de ${nome()} é $media.")
     retorno()
 }
